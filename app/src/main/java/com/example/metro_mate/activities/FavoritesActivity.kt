0d80dccaa898a0a93786c5e1e81cc9608c0526a4
Hojin Jeong <60232061@mju.ac.kr>
2024-11-21 21:40:51 +0900

    Second Merge

--- app/src/main/java/com/example/metro_mate/activities/FavoritesActivity.kt
+++ app/src/main/java/com/example/metro_mate/activities/FavoritesActivity.kt
@@ -2,7 +2,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class FavoritesActivity : AppCompatActivity() {
-    protected fun onCreate(savedInstanceState: Bundle?) {
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // Logic for managing favorite routes
     }
