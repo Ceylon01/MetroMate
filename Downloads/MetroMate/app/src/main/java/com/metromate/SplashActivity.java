c4de2634f75344fe25ab40d8932ff43ccecf6fbb
Hojin Jeong <60232061@mju.ac.kr>
2024-11-27 00:15:25 +0900

    Java-Branch

---
+++ Downloads/MetroMate/app/src/main/java/com/metromate/SplashActivity.java
@@ -0,0 +1,22 @@
+package com.metromate;
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // 스플래시 레이아웃 설정
+        setContentView(R.layout.activity_splash);
+
+        // 일정 시간 뒤에 MainActivity로 이동
+        new android.os.Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, 3000); // 3000ms = 3초
+    }
+}
