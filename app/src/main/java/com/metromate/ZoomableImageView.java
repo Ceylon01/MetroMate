384311360fb4b6e78def155e105eb43569340dad
kookjimin <kook0707@mju.ac.kr>
2024-12-10 23:56:27 +0900

    맵 수정 완료

--- app/src/main/java/com/metromate/ZoomableImageView.java
+++ app/src/main/java/com/metromate/ZoomableImageView.java
@@ -47,6 +47,10 @@ public class ZoomableImageView extends AppCompatImageView {
                 float dx = event.getX() - lastTouch.x;
                 float dy = event.getY() - lastTouch.y;
                 matrix.postTranslate(dx, dy);
+
+                // 이미지가 화면 밖으로 벗어나지 않도록 제한
+                limitMovement();
+
                 setImageMatrix(matrix);
                 lastTouch.set(event.getX(), event.getY());
                 break;
@@ -54,9 +58,42 @@ public class ZoomableImageView extends AppCompatImageView {
         return true;
     }
 
+    private void limitMovement() {
+        // 현재 매트릭스 값 가져오기
+        matrix.getValues(matrixValues);
+
+        // 이미지를 확대, 축소 후의 크기 계산
+        float currentScale = matrixValues[Matrix.MSCALE_X]; // scaleX 값 (이미지 크기)
+        float imageWidth = getDrawable().getIntrinsicWidth() * currentScale;
+        float imageHeight = getDrawable().getIntrinsicHeight() * currentScale;
+
+        // 화면의 여유 공간을 10%로 설정
+        float marginX = getWidth() * 0.2f;
+        float marginY = getHeight() * 0.2f;
+
+        // 이미지의 이동 범위를 제한 (이미지가 화면을 벗어나지 않도록)
+        float maxX = marginX;
+        float maxY = marginY;
+        float minX = getWidth() - imageWidth - marginX;
+        float minY = getHeight() - imageHeight - marginY;
+
+        // 이동을 제한
+        if (matrixValues[Matrix.MTRANS_X] > maxX) {
+            matrix.postTranslate(maxX - matrixValues[Matrix.MTRANS_X], 0);
+        } else if (matrixValues[Matrix.MTRANS_X] < minX) {
+            matrix.postTranslate(minX - matrixValues[Matrix.MTRANS_X], 0);
+        }
+
+        if (matrixValues[Matrix.MTRANS_Y] > maxY) {
+            matrix.postTranslate(0, maxY - matrixValues[Matrix.MTRANS_Y]);
+        } else if (matrixValues[Matrix.MTRANS_Y] < minY) {
+            matrix.postTranslate(0, minY - matrixValues[Matrix.MTRANS_Y]);
+        }
+    }
+
     public void resetZoom() {
         matrix.reset();
-        matrix.postScale(0.3f, 0.3f);
+        matrix.postScale(minZoom, minZoom);
         setImageMatrix(matrix);
     }
 
@@ -64,9 +101,24 @@ public class ZoomableImageView extends AppCompatImageView {
         @Override
         public boolean onScale(ScaleGestureDetector detector) {
             float scaleFactor = detector.getScaleFactor();
+
+            // 스케일 제한 (최소, 최대 줌 크기)
+            if (scaleFactor * getCurrentScale() > maxZoom) {
+                scaleFactor = maxZoom / getCurrentScale();
+            } else if (scaleFactor * getCurrentScale() < minZoom) {
+                scaleFactor = minZoom / getCurrentScale();
+            }
+
             matrix.postScale(scaleFactor, scaleFactor, detector.getFocusX(), detector.getFocusY());
+            limitMovement();  // 이동 범위 제한
             setImageMatrix(matrix);
+
             return true;
         }
+    }
+
+    private float getCurrentScale() {
+        matrix.getValues(matrixValues);
+        return matrixValues[Matrix.MSCALE_X]; // 현재 스케일 값
     }
 }
